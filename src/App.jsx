4722423e5c022171232e74d27a917b9dd0cf5f76
ashishverma2831 <ashishverma510207@gmail.com>
2024-02-14 00:23:37 +0530

    Add Accordion component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Tabs from './components/Tabs'
+import Accordion from './components/Accordion'
 
 const App = () => {
   return (
     <>
-      <Tabs />
+      {/* <Tabs /> */}
+      <Accordion />
     </>
   )
 }
