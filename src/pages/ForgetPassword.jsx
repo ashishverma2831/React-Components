2f5c5da2bd7d2a380bee765c955117aca58219a8
ashishverma2831 <ashishverma510207@gmail.com>
2024-07-10 21:12:54 +0530

    chore: Update ForgetPassword page layout and styles

--- src/pages/ForgetPassword.jsx
+++ src/pages/ForgetPassword.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ForgetPassword = () => {
     return (
         <>
-            <section style={{ backgroundImage: 'linear-gradient(to right top, #72a5f1, #4dc3eb, #7cd7d4, #bae5c8, #edf0d5)' }} className='h-screen'>
+            <section style={{ backgroundImage: 'linear-gradient(to right top, #72a5f1, #4dc3eb, #7cd7d4, #bae5c8, #edf0d5)' }} >
                 <header className='flex justify-between p-4'>
                     <img width={40} className='cursor-pointer mix-blend-color-burn' src='https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQgByBT5IiAT_a2x9pUVb4VMoOrlzHH7Jrzj-HB5jzHlR4lNLMS' />
                     <div className='flex gap-4 items-center'>
@@ -11,11 +11,11 @@ const ForgetPassword = () => {
                         <i className="cursor-pointer animate-spin text-center rounded-full fa-solid fa-gear text-xl text-gray-500"></i>
                     </div>
                 </header>
-                <div className=' my-10 p-5'>
+                <div className='h-screen flex justify-center items-center px-2 sm:p-5'>
                     <div className='w-[450px] rounded-2xl mx-auto text-center flex justify-center items-center flex-col gap-2 bg-white p-5 shadow-lg'>
-                        <img width={100} className='my-5 bg-transparent' src='https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQgByBT5IiAT_a2x9pUVb4VMoOrlzHH7Jrzj-HB5jzHlR4lNLMS' />
+                        <img width={100} className='sm:my-5 my-2 bg-transparent' src='https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQgByBT5IiAT_a2x9pUVb4VMoOrlzHH7Jrzj-HB5jzHlR4lNLMS' />
                         <p className='text-xl font-medium'>Forget your password?</p>
-                        <p className='text-[16px] text-gray-500'>Please enter the email address associated with your account and we'll email you a link to reset your password.</p>
+                        <p className='sm:text-[16px] text-[14px] text-gray-500'>Please enter the email address associated with your account and we'll email you a link to reset your password.</p>
                         <form className='w-full mt-8 my-4 relative'>
                             <input className='py-3 rounded-lg px-4 border-2 w-full ' type='email' placeholder='example@gmail.com' />
                             <label className='text-sm bg-white px-2 font-semibold absolute -top-3 text-gray-700 left-2'>Email address</label>
